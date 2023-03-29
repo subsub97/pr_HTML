3c1267090fdf7dc83b21c4cfaf5f8fd09b80a282
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2023-03-29 17:09:29 +0000

    Add files via upload
    please check my code

---
+++ HW2_2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include<fcntl.h>
+#include<sys/wait.h>
+
+int main(int argc,char *argv[])
+{
+    int fd1[2],fd2[2];
+    int input,output;
+    int n; char buf[10];
+
+    pipe(fd1);
+    pipe(fd2);
+    if(fork() == 0){//child1 process
+        close(fd1[1]);
+        close(fd2[0]);
+        while((n = read(fd1[0],buf,10)) != 0){
+            for(int i=0;i < n; ++i){ //change upper alpa
+                buf[i] = toupper(buf[i]);
+            }
+            write(fd2[1],buf,n);
+        }
+        close(fd1[0]);
+        close(fd2[1]);
+        exit(0);
+        }
+    else{
+        if(fork() == 0){
+            close(fd1[0]);
+            close(fd1[1]);
+            close(fd2[1]);
+            output = creat("output2.dat",0666);
+            while(( n = read(fd2[0],buf,10) != 0)){
+                write(output,buf,n);}
+            close(output);
+            close(fd2[0]);
+            exit(1);
+                
+        }
+        else{ //parent process
+            input = open("input2.dat",0); //open file "input.dat" for reading
+            close(fd1[0]);
+            close(fd2[1]);
+            close(fd2[0]);
+            while(( n = read(input,buf,10) != 0)){
+                write(fd1[1],buf,n);
+            }
+            close(input);
+            close(fd1[1]);
+            
+
+            wait(NULL);
+
+        }
+    }
+    return 0;
+    }
